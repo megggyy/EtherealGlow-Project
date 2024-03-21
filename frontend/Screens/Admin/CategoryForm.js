b7a19f7faef0744d96d049c5af6892206a2c2dac
megggyy <shanaimeg.honrado@tup.edu.ph>
2024-03-21 16:35:42 +0800

    category create

---
+++ frontend/Screens/Admin/CategoryForm.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from "react"
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    Platform
+} from "react-native"
+import { Item, Picker, Select, Box } from "native-base"
+import FormContainer from "../../Shared/Form/FormContainer"
+import Input from "../../Shared/Form/Input"
+import EasyButton from "../../Shared/StyledComponents/EasyButton"
+
+import Icon from "react-native-vector-icons/FontAwesome"
+import Toast from "react-native-toast-message"
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import baseURL from "../../assets/common/baseurl"
+import Error from "../../Shared/Error"
+import axios from "axios"
+import * as ImagePicker from "expo-image-picker"
+import { useFocusEffect, useNavigation } from "@react-navigation/native"
+import mime from "mime";
+
+
+const CategoryForm = (props) => {
+   
+    const [categories, setCategories] = useState([]);
+    const [categoryName, setCategoryName] = useState();
+    const [token, setToken] = useState();
+    const [error, setError] = useState();
+    const [item, setItem] = useState(null);
+    let navigation = useNavigation()
+
+    useEffect(() => {
+        if (!props.route.params) {
+            setItem(null);
+        } else {
+            setItem(props.route.params.item);
+            setCategoryName(props.route.params.item.name);
+
+        }
+        AsyncStorage.getItem("jwt")
+            .then((res) => {
+                setToken(res);
+            })
+            .catch((error) => console.log(error));
+
+        return () => {
+            setToken();
+        }
+    }, [])
+    
+
+  const addCategory = () => {
+
+    const category = {
+            name: categoryName
+        };
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        };
+
+        axios
+            .post(`${baseURL}categories`, category, config)
+            .then((res) => {
+                if (res.status === 200 || res.status === 201) {
+                    Toast.show({
+                        topOffset: 60,
+                        type: "success",
+                        text1: "New Category added",
+                        text2: ""
+                    });
+                    setTimeout(() => {
+                        navigation.navigate("Categories");
+                    }, 500)
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+                Toast.show({
+                    topOffset: 60,
+                    type: "error",
+                    text1: "Something went wrong",
+                    text2: "Please try again"
+                })
+            })
+
+        // setCategoryName("");
+        
+    }
+    
+    
+
+    
+    return (
+        <FormContainer title="Add Category">
+            <View style={styles.label}>
+                <Text style={{ textDecorationLine: "underline" }}>Name</Text>
+            </View>
+            <Input
+                placeholder="Name"
+                name="name"
+                id="name"
+                value={categoryName}
+                onChangeText={(text) => setCategoryName(text)}
+            />
+
+            {error ? <Error message={error} /> : null}
+            <View style={styles.buttonContainer}>
+                <EasyButton
+                    large
+                    primary
+                    onPress={() => addCategory()}
+                ><Text style={styles.buttonText}>Confirm</Text>
+                </EasyButton>
+            </View>
+            
+        </FormContainer>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    label: {
+        width: "80%",
+        marginTop: 10
+    },
+    buttonContainer: {
+        width: "80%",
+        marginBottom: 80,
+        marginTop: 20,
+        alignItems: "center"
+    },
+    buttonText: {
+        color: "white"
+    },
+    imageContainer: {
+        width: 200,
+        height: 200,
+        borderStyle: "solid",
+        borderWidth: 8,
+        padding: 0,
+        justifyContent: "center",
+        borderRadius: 100,
+        borderColor: "#E0E0E0",
+        elevation: 10
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 100
+    },
+    imagePicker: {
+        position: "absolute",
+        right: 5,
+        bottom: 5,
+        backgroundColor: "grey",
+        padding: 8,
+        borderRadius: 100,
+        elevation: 20
+    }
+})
+
+
+export default CategoryForm;
