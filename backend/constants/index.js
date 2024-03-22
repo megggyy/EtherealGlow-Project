43cb8ec24668ce1065cceabe48ed4075c2894470
megggyy <shanaimeg.honrado@tup.edu.ph>
2024-03-22 16:58:15 +0800

    add constants

---
+++ backend/constants/index.js
@@ -0,0 +1,9 @@
+const RESOURCE = require("./resource");
+const STATUSCODE = require("./statusCode");
+const ERROR = require("./error");
+
+module.exports = {
+  RESOURCE,
+  STATUSCODE,
+  ERROR,
+};
---
+++ backend/constants/resource.js
@@ -0,0 +1,9 @@
+module.exports = {
+  AUTH: "auth",
+  IMAGES: "images",
+  IMAGE: "image",
+  PRODUCTION: "production",
+  NONE: "none",
+  JWT: "jwt",
+  USER: "user",
+};
