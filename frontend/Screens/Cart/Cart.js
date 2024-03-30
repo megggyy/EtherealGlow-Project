ff6d6306d18045f43e5194660ada3872748e6b1a
megggyy <shanaimeg.honrado@tup.edu.ph>
2024-03-30 16:19:39 +0800

    refactor cart

--- frontend/Screens/Cart/Cart.js
+++ frontend/Screens/Cart/Cart.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useNavigation } from '@react-navigation/native';
 import Icon from "react-native-vector-icons/FontAwesome";
 import { SwipeListView } from 'react-native-swipe-list-view';
+import { UPDATE_CART } from '../../Redux/constants';
 import { removeFromCart, clearCart } from '../../Redux/Actions/cartActions'
 var { height, width } = Dimensions.get("window");
 import EasyButton from "../../Shared/StyledComponents/EasyButton"
@@ -15,44 +16,89 @@ const Cart = () => {
     const navigation = useNavigation()
     const dispatch = useDispatch()
     const cartItems = useSelector(state => state.cartItems)
+    
     const context = useContext(AuthGlobal)
-    var total = 0;
+    var totalPrice = 0;
     console.log("cart", cartItems)
     cartItems.forEach(cart => {
-        return (total += cart.price)
+        totalPrice += cart.price * cart.quantity;
     });
-    const renderItem = ({ item, index }) =>
-        <TouchableHighlight
-            _dark={{
-                bg: 'coolGray.800'
-            }}
-            _light={{
-                bg: 'white'
-            }}
 
-        >
-            <Box pl="4" pr="5" py="2" bg="white" keyExtractor={item => item.id}>
-                <HStack alignItems="center" space={3}>
-                    <Avatar size="48px" source={{
-                        uri: item.image ?
-                            item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
-                    }} />
-                    <VStack>
-                        <Text color="coolGray.800" _dark={{
-                            color: 'warmGray.50'
-                        }} bold>
-                            {item.name}
-                        </Text>
-                    </VStack>
-                    <Spacer />
-                    <Text fontSize="xs" color="coolGray.800" _dark={{
-                        color: 'warmGray.50'
-                    }} alignSelf="flex-start">
-                        $ {item.price}
+    const renderItem = ({ item, index }) => {
+        const getFirstImage = () => {
+            if (Array.isArray(item.image) && item.image.length > 0) {
+                return item.image[0].url;
+            }
+            return 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png';
+        };
+        return (
+            <TouchableHighlight
+                _dark={{
+                    bg: 'coolGray.800'
+                }}
+                _light={{
+                    bg: 'white'
+                }}
+            >
+                <Box pl="4" pr="5" py="2" bg="white" keyExtractor={item => item.product}>
+                    <HStack alignItems="center" space={3}>
+                        <Avatar size="48px" source={{ uri: getFirstImage() }} />
+                        <VStack>
+                            <Text color="coolGray.800" _dark={{ color: 'warmGray.50' }} bold>
+                                {item.name}
+                            </Text>
+                        </VStack>
+                        <HStack space={2}>
+                        <TouchableOpacity onPress={() => handleQuantityChange(dispatch, cartItems, item, 'decrease')}>
+                            <Icon name="minus" size={20} />
+                        </TouchableOpacity>
+                        <Text>{item.quantity}</Text>
+                        <TouchableOpacity onPress={() => handleQuantityChange(dispatch, cartItems, item, 'increase')}>
+                            <Icon name="plus" size={20} />
+                        </TouchableOpacity>
+                    </HStack>
+                    <Text fontSize="xs" color="coolGray.800" _dark={{ color: 'warmGray.50' }} alignSelf="flex-start">
+                        $ {item.price * item.quantity}
                     </Text>
                 </HStack>
             </Box>
-        </TouchableHighlight>;
+        </TouchableHighlight>
+        );
+    };
+
+    const handleQuantityChange = (dispatch, cartItems, item, action) => {
+        // Find the index of the item in the cart
+        const itemIndex = cartItems.findIndex(cartItem => cartItem.id === item.id);
+    
+        // Create a copy of the cart items array to avoid mutating the original state
+        const updatedCartItems = [...cartItems];
+    
+        // If the item is found in the cart
+        if (itemIndex !== -1) {
+            // If action is 'increase', increase the quantity by 1
+            if (action === 'increase') {
+                updatedCartItems[itemIndex].quantity += 1;
+            }
+            // If action is 'decrease' and quantity is greater than 1, decrease the quantity by 1
+            else if (action === 'decrease' && updatedCartItems[itemIndex].quantity > 1) {
+                updatedCartItems[itemIndex].quantity -= 1;
+            }
+    
+            // Dispatch an action to update the cart with the updated cart items
+            dispatch({ type: UPDATE_CART, payload: updatedCartItems });
+        } else {
+            // If the item is not found in the cart, add it to the cart with product ID
+            const newItem = {
+                ...item,
+                id: item.product._id, // Assuming the product ID is stored in "_id"
+                quantity: 1 // Assuming the default quantity is 1
+            };
+            dispatch({ type: ADD_TO_CART, payload: newItem }); // Dispatch action to add to cart
+        }
+    };
+    
+    
+    
 
     const renderHiddenItem = (cartItems) =>
         <TouchableOpacity
@@ -93,7 +139,7 @@ const Cart = () => {
             <VStack style={styles.bottomContainer} w='100%' justifyContent='space-between'
             >
                 <HStack justifyContent="space-between">
-                    <Text style={styles.price}>$ {total.toFixed(2)}</Text>
+                    <Text style={styles.price}>$ {totalPrice.toFixed(2)}</Text>
                 </HStack>
                 <HStack justifyContent="space-between">
                     {/* <Button alignItems="center" onPress={() => dispatch(clearCart())} >Clear</Button> */}
@@ -106,10 +152,7 @@ const Cart = () => {
                         <Text style={{ color: 'white' }}>Clear</Text>
                     </EasyButton>
                 </HStack>
-                {/* <HStack justifyContent="space-between">
-                   
-                    <Button alignItems="center" colorScheme="primary" onPress={() => navigation.navigate('Checkout')}>Check Out</Button>
-                </HStack> */}
+              
                 {context.stateUser.isAuthenticated ? (
                     <EasyButton
                         primary
