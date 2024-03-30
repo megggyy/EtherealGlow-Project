31f9fb1eef713f2cb55f72990dfc40ae33a29622
megggyy <shanaimeg.honrado@tup.edu.ph>
2024-03-30 16:19:13 +0800

    add shipping

--- frontend/Navigators/CheckoutNavigator.js
+++ frontend/Navigators/CheckoutNavigator.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
 // Screens
-import Checkout from '../Screens/Cart/Checkout/Checkout'
+import Shipping from '../Screens/Cart/Checkout/Shipping'
 import Payment from '../Screens/Cart/Checkout/Payment'
 import Confirm from '../Screens/Cart/Checkout/Confirm';
 
@@ -11,7 +11,7 @@ const Tab = createMaterialTopTabNavigator();
 function MyTabs() {
     return(
         <Tab.Navigator>
-            <Tab.Screen name="Shipping" component={Checkout} />
+            <Tab.Screen name="Shipping" component={Shipping} />
             <Tab.Screen name="Payment" component={Payment} />
             <Tab.Screen name="Confirm" component={Confirm} />
         </Tab.Navigator>
