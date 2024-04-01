bc8ea03c248d79ed691a5a665239497d968f90b9
megggyy <shanaimeg.honrado@tup.edu.ph>
2024-04-01 14:38:30 +0800

    add userchart route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const { uploadOptions } = require('../utils/cloudinary');
 
+router.get('/usersPerMonth', userController.getUsersPerMonth);
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
 router.post('/', userController.createUser);
@@ -11,6 +12,7 @@ router.post('/login', userController.loginUser);
 router.post('/register', uploadOptions.single('image'), userController.registerUser);
 router.delete('/:id', userController.deleteUser);
 router.get('/get/count', userController.getUserCount);
+
 
 module.exports = router;
 
