7d8829f8813d5d6ed007a35631415ff640ad9bd9
megggyy <shanaimeg.honrado@tup.edu.ph>
2024-04-01 21:41:39 +0800

    add admin navigator for user

--- frontend/Navigators/AdminNavigator.js
+++ frontend/Navigators/AdminNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack"
 
 import Orders from "../Screens/Admin/Orders"
 import Products from "../Screens/Admin/Products"
+import Users from "../Screens/Admin/Users"
 import ProductForm from "../Screens/Admin/ProductForm"
 import CategoryForm from "../Screens/Admin/CategoryForm"
 import CategoryEdit from "../Screens/Admin/CategoryEdit"
@@ -24,6 +25,7 @@ const AdminNavigator= () => {
             />
             <Stack.Screen name="Categories" component={Categories} />
             <Stack.Screen name="Orders" component={Orders} />
+            <Stack.Screen name="Users" component={Users} />
             <Stack.Screen name="ProductForm" component={ProductForm} />
             <Stack.Screen name="CategoryForm" component={CategoryForm} />
             <Stack.Screen name="CategoryEdit" component={CategoryEdit} />
